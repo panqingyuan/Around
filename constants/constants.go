6c2d52e23b3fc929d3bfeb269cd57e76f38923db
panqingyuan <qingyuan25@g.ucla.edu>
2023-01-20 00:28:31 -0800

    backend

---
+++ constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+	USER_INDEX  = "user"
+	POST_INDEX  = "post"
+	ES_URL      = "http://10.128.0.2:9200"
+	ES_USERNAME = "qingyuan"
+	ES_PASSWORD = "p123456"
+	GCS_BUCKET  = "around-bucket-20221019"
+)
